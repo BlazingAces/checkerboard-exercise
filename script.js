fda20c6b95750db1941597b7acb499a18c066dac
BlazingAces <1002charles@gmail.com>
2022-06-23 19:10:16 -0400

    Colors

--- script.js
+++ script.js
@@ -5,41 +5,41 @@
 //      Set each tile's float property to left
 //      Each tile's paddingBottom is 11.1%
 
-let body = document.body; //Use getElementsByTagName to get the body tag. Can add [0] if they is more then one of the same tag
+// let body = document.body; //Use getElementsByTagName to get the body tag. Can add [0] if they is more then one of the same tag
 
-function checkerBoard() {
-    for (var i = 0; i < 63; i++) {
-        let square = document.createElement('div') 
-        square.style.width = "11%";
-        square.style.float = "left";
-        square.style.paddingBottom = "11%";
-        document.body.append(square);
+// function checkerBoard() {
+//     for (var i = 0; i < 63; i++) {
+//         let square = document.createElement('div') 
+//         square.style.width = "11%";
+//         square.style.float = "left";
+//         square.style.paddingBottom = "11%";
+//         document.body.append(square);
 
 
-    if (i % 2 !== 0) {
-        square.style.backgroundColor = 'red';
-    } else {
-        square.style.backgroundColor = 'black';
-        }
-    }
-}
+//     if (i % 2 !== 0) {
+//         square.style.backgroundColor = 'red';
+//     } else {
+//         square.style.backgroundColor = 'black';
+//         }
+//     }
+// }
 
-checkerBoard();
+// checkerBoard();
 
-// let body = document.body;
+let body = document.body;
 
-// function randomColor() {
+function randomColor() {
 
-// for (var i = 0; i < 63; i++) {
-//     let randomOne = Math.ceil(Math.random() * 255)
-//     let randomTwo = Math.ceil(Math.random() * 255)
-//     let randomThree = Math.ceil(Math.random() * 255)
-//     let square = document.createElement('div') 
-//     square.style.width = "11.1%";
-//     square.style.float = "left";
-//     square.style.paddingBottom = "11.1%";
-//     document.body.append(square);
-//     square.style.background = `rgb(${randomOne},${randomTwo},${randomThree})`;
+for (var i = 0; i < 63; i++) {
+    let randomOne = Math.ceil(Math.random() * 255)
+    let randomTwo = Math.ceil(Math.random() * 255)
+    let randomThree = Math.ceil(Math.random() * 255)
+    let square = document.createElement('div') 
+    square.style.width = "11.1%";
+    square.style.float = "left";
+    square.style.paddingBottom = "11.1%";
+    document.body.append(square);
+    square.style.background = `rgb(${randomOne},${randomTwo},${randomThree})`;
     // console.log(randomColorOne);
 
 
@@ -48,7 +48,7 @@ checkerBoard();
 // } else {
 //     square.style.backgroundColor = rgbA;
 //     }
-//  }
-// }
+ }
+}
 
-// randomColor();
+randomColor();
